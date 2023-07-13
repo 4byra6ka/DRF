e2f97e7f643d7cf847834bd0a14976e916169aba
gau <gau@1jz.ru>
2023-07-13 11:33:58 +0300

    fin

--- course/urls.py
+++ course/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 
 from course.apps import CourseConfig
 from course.serializers import LessonSerializer
-from course.views import *
+from course.views import LessonListAPIView, LessonDetailAPIView, LessonCreateAPIView, LessonDestroyAPIView, \
+    LessonUpdateAPIView, CourseViewSet
 
 app_name = CourseConfig.name
 
@@ -12,16 +13,15 @@ router.register(r'course', CourseViewSet)
 
 urlpatterns = [
 
-    path('lesson/', LessonListAPIView.as_view()),
-    # path('lesson/', ListAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-list'),
-    path('lesson/<int:pk>/', LessonDetailAPIView.as_view()),
-    # path('lesson/<int:pk>/', RetrieveAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-detail'),
-    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view()),
-    # path('lesson/<int:pk>/update/', UpdateAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-update'),
-    path('lesson/create/', LessonCreateAPIView.as_view()),
-    # path('lesson/create/', CreateAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-create'),
-    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view()),
-    # path('lesson/<int:pk>/delete/', DestroyAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-delete'),
+                  path('lesson/', LessonListAPIView.as_view()),
+                  # path('lesson/', ListAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-list'),
+                  path('lesson/<int:pk>/', LessonDetailAPIView.as_view()),
+                  # path('lesson/<int:pk>/', RetrieveAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-detail'),
+                  path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view()),
+                  # path('lesson/<int:pk>/update/', UpdateAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-update'),
+                  path('lesson/create/', LessonCreateAPIView.as_view()),
+                  # path('lesson/create/', CreateAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-create'),
+                  path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view()),
+                  # path('lesson/<int:pk>/delete/', DestroyAPIView.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-delete'),
 
-] + router.urls
-
+              ] + router.urls
